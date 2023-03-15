fdc2d51c7814fb082bd8f55c9128fbe2beec7d76
andy091954@gmail.com <andy0124@hanmail.net>
2023-03-16 00:05:17 +0900

    풀이 진행중

--- 2022/03/oddHoly.py
+++ 2022/03/oddHoly.py
@@ -3,26 +3,24 @@ class test:
     def __init__(self):
         self.min = 10000000
         self.max = 0
-    def solution(input_num):
+    def solution(self,input_num):
         answer = []
-        min_num = 1000000000000
-        max_num = 0
-        self.repeat(self,input_num,0,min_num,max_num)
-        print(min_num)
-        print(max_num)
+        self.repeat(input_num,0)
+        answer.append(self.min)
+        answer.append(self.max)
 
         #입력을 받아 온다.
         # 재귀 돌리면서 최소 최대 를 파악한다.
         return answer
 
 ##
-    def repeat(self,num, cnt, min, max):
+    def repeat(self,num, cnt):
         #숫자가 몇자리수 인지 파악하고 한자리수면 return 하면서 결과값을 보낸다.
         if int(num / 10) == 0 :
             temp_num = cnt
             if num % 2 == 1: temp_num = temp_num + 1
-            if temp_num > max : max = temp_num
-            if temp_num < min : min = temp_num
+            if temp_num > self.max : self.max = temp_num
+            if temp_num < self.min : self.min = temp_num
             return
 
         #2자리수면 그 두개를 합치고 다시 repeat 한다.
@@ -33,18 +31,24 @@ class test:
             second = num % 10
             if second % 2 ==1 : temp_cnt = temp_cnt + 1
             temp_num = first + second
-            self.repeat(self,temp_num, temp_cnt,min,max)
+            self.repeat(temp_num, temp_cnt)
             return
 
         #3자리 이상이면 이때 부터 분리 시점을 파악한다.
         #숫자를 분리한다.
-
+        temp_num_array = []
+        temp_num = num
+        while temp_num // 10 > 0 :
+            temp_num_array.append(temp_num % 10)
+            temp_num = temp_num // 10
+        temp_num_array = temp_num_array.reverse()
+        
 
 
 
 
 if __name__ == '__main__' :
-    ttt = test
+    ttt = test()
     result = ttt.solution(12)
     print(result)
 
